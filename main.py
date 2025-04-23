5e9641defd5e43c40ccc50e7ef1d880f566ffd48
[yuan dev] <[niannian2024@yeah.net]>
2025-04-23 20:03:04 +0800

    2025 - 4 -23

--- main.py
+++ main.py
@@ -16,10 +16,13 @@ SCREEN_SIZE = (SCREEN_WIDTH,SCREEN_HIGHT)
 full_tag = pygame.FULLSCREEN|pygame.SCALED
 test_tag = pygame.SCALED|pygame.RESIZABLE 
 screen = pygame.display.set_mode(SCREEN_SIZE,flags = test_tag)
+clock = pygame.time.Clock()
 cx=0
 cy=0
 tcx=0
 tcy=0
+pcx=1
+pcy=1
 
 with open('assets/map1.json') as json_file:
     data = json.load(json_file)
@@ -31,7 +34,7 @@ for i in range(75):
         "x":i,
         "y":j
     }
-print(test)
+# print(test)
 pen = render.RenderPen(screen)
 tile_map = tiles.TileGrid(test)
 
@@ -42,17 +45,22 @@ while(True):
             sys.exit()
 
     key = pygame.key.get_pressed()
+    pcx=cx
+    pcy=cy
     cx += (tcx - cx)*0.3
     # print(key[pygame.K_a])
     # print(cx)
     if(key[pygame.K_a]):
         tcx -= 5
-    
-    screen.fill("#21263f")
 
     tile_state = tile_map.get_pos(cx,cy)
-    pen.draw("block",tile_state)
+    # print(pcx)
+    # print(cx)
+    if(round(pcx) != round(cx) or round(pcy) != round(cy) or clock.get_time()<10):
+        screen.fill("#21263f")
+        pen.draw("block",tile_state)
     
-    
+    print(clock.get_fps())
     pygame.display.flip()
     pygame.display.update()
+    clock.tick(60)
